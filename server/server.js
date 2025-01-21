451342319c46ec6e4805e9bfe8fa78913db75c69
aRonnieAlsop <alsop.arron@gmail.com>
2025-01-20 20:18:45 -0800

    Serve React app in production

--- server/server.js
+++ server/server.js
@@ -23,6 +23,17 @@ mongoose
 // Routes
 app.use("/api/blogs", blogRoutes);
 
+// Serve static files from the React app in production
+if (process.env.NODE_ENV === 'production') {
+  // Set the React build folder as static files
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+  // Catch-all route to serve the React app for any other route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 // Fallback route for undefined routes
 app.use((req, res) => {
