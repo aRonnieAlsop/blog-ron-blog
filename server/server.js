27f9a218707b0b57d13eeae332940a614034f692
aRonnieAlsop <alsop.arron@gmail.com>
2025-01-20 21:39:00 -0800

    'wip'

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const blogRoutes = require("./routes/blogRoutes");
-
+const path = require('path'); 
 
 dotenv.config();
 
